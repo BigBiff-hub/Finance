d783a720afddc9b043b6397ab8142a01771316ad
BigBiff-hub <77340730+BigBiff-hub@users.noreply.github.com>
2021-10-18 22:39:32 +0100

    -> able to stream ETHER prices from websocket

--- bot.py
+++ bot.py
@@ -1,16 +1,34 @@
 import json
 import pprint
-
+from binance.client import Client
+from binance.enums import *
+import config
 import websocket, talib, numpy
 
+
+client = Client(config.API_KEY, config.API_SECRET, tld='us')
 SOCKET = "wss://stream.binance.com:9443/ws/ethusdt@kline_1m"
+
+
 RSI_PERIOD = 14
 RSI_OVERBOUGHT = 70
 RSI_OVERSOLD = 30
 TRADE_SYM = 'ETHUSD'
-TRADE_QUANTITIY = 0.05
+TRADE_QUANTITIY = 0.0005
+
 closes = []
 in_position = False
+
+def order(side, quantity, symbol, order_type= ORDER_TYPE_MARKET):
+    try:
+        print('sending order')
+        order = client.create_order(symbol=symbol, side=side, type=order_type, quantity=quantity)
+        print(order)
+
+    except Exception as e:
+        print('order failed')
+        return False
+    return True
 
 
 def on_open(ws):
@@ -22,38 +40,50 @@ def on_close(ws):
 
 
 def on_message(ws, message):
+    global closes, in_position
     json_message = json.loads(message)
     pprint.pprint(json_message)
-    candle = json_message["k"]
-    is_candled_closed = json_message["x"]
-    close = json_message["c"]
+    candle = json_message['k']
 
-    if is_candled_closed:
-        print(f"candle closed at {close}")
+    is_candle_closed = candle['x']
+    close = candle['c']
+
+    if is_candle_closed:
+        print("candle closed at {}".format(close))
         closes.append(float(close))
         print("closes")
         print(closes)
 
         if len(closes) > RSI_PERIOD:
-            num_closes = numpy.array(closes)
-            rsi = talib.RSI(num_closes, RSI_PERIOD)
-            print('all rsis calculated so far')
+            np_closes = numpy.array(closes)
+            rsi = talib.RSI(np_closes, RSI_PERIOD)
+            print("all rsis calculated so far")
             print(rsi)
             last_rsi = rsi[-1]
-            print(f'the current rsi is {last_rsi}')
+            print("the current rsi is {}".format(last_rsi))
 
             if last_rsi > RSI_OVERBOUGHT:
                 if in_position:
-                    print('sell now ethereum is overbought')
-                    # TODO: put binance sell logic here
+                    print("Overbought! Sell! Sell! Sell!")
+                    # put binance sell logic here
+                    order_succeeded = order(SIDE_SELL, TRADE_QUANTITIY, TRADE_SYM)
+                    if order_succeeded:
+                        in_position = False
                 else:
-                    print("No shares currently available cannot do anything")
+                    print("It is overbought, but we don't own any. Nothing to do.")
+
             if last_rsi < RSI_OVERSOLD:
                 if in_position:
-                    print("its oversold, but you already own it cannot do anything")
+                    print("It is oversold, but you already own it, nothing to do.")
                 else:
-                     print('Buy now as ethereum is over sold')
-                    #TODO: put binance logic here
+                    print("Oversold! Buy! Buy! Buy!")
+                    # put binance buy order logic here
+                    order_succeeded = order(SIDE_BUY, TRADE_QUANTITIY, TRADE_SYM)
+                    if order_succeeded:
+                        in_position = True
+
+
+
 
 
 
